0a465ce1b77cb7df5e1889f12f1bf7c99d2d9899
DivyeSharma <114800194+DivyeSharma@users.noreply.github.com>
2022-11-05 13:24:38 +0530

    Add files via upload

---
+++ Comapany_bonus4.py
@@ -0,0 +1,9 @@
+age1 = int(input("Enter the Age of Ram :"))
+age2 = int(input("Enter the Age of Shyam :"))
+age3 = int(input("Enter the Age of Ajay :"))
+if(age1<age2 and age1<age3):
+	print("The Youngest Age is Ram")
+elif(age2<age1 and age2<age3):
+	print("The Youngest Age is Shyam")
+else:
+	print("The Youngest Age is Ajay")
