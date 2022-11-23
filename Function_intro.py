f6ab83d9a56b98b8606eee35a1f6508274a72544
DivyeSharma <114800194+DivyeSharma@users.noreply.github.com>
2022-11-23 17:07:44 +0530

    Add files via upload

---
+++ Function_intro.py
@@ -0,0 +1,27 @@
+#def multiply(num1,num2):
+    #sum = num1 * num2
+    #print(sum)
+    #return sum
+#multiply(4,5)
+
+
+
+#def name(Firstname="Vishal"):
+#    print("My name is : " +Firstname)  
+#name("Teja")
+
+
+#Keyword variable length argument
+#for detail()
+
+#    print(name)
+#for i,j in data.items():
+#    print(1,j)
+#details
+ 
+def factorial(num):
+    if num==0:
+        return 1
+    return num * factorial(num-1)
+a = factorial(6)
+print(a)
